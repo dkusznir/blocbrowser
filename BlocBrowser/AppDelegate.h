a8fa5af401ee61555f417451542df94b26edf958
Dorian Kusznir <doriank87@gmail.com>
2015-03-08 23:33:39 -0400

    Initial Commit

---
+++ BlocBrowser/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BlocBrowser
+//
+//  Created by Dorian Kusznir on 3/8/15.
+//  Copyright (c) 2015 Bloc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
